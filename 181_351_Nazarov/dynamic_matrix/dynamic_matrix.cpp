0390a1e2db037cc7cd41d30f2607d6108cc58582
spoff <evenstruct0@gmail.com>
2019-01-15 22:37:49 +0300

    Vector

--- 181_351_Nazarov/dynamic_matrix/dynamic_matrix.cpp
+++ 181_351_Nazarov/dynamic_matrix/dynamic_matrix.cpp
@@ -1,242 +1,275 @@
-﻿// dynamic_matrix.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
-
 class Matrix {
-private:
-	double **matr;
+	double **mat;
 	int rows, columns;
 public:
 	Matrix();
+	Matrix(int n_rows, int n_cols);
+	Matrix(int n_cols)
+	{
+		rows = 1;
+		columns = n_cols;
+	}
 	~Matrix();
-
+	friend Matrix operator+(Matrix& a, Matrix& b);
+	friend Matrix operator-(Matrix& a, Matrix& b);
+	friend Matrix operator*(Matrix& a, Matrix& b);
+	friend std::ostream& operator<<(std::ostream &os, Matrix& b);
 	bool input();
 	bool print();
-	bool summMatrix(Matrix matr2);
-	bool multMatrix(Matrix matr2);
+	bool summMatrix(Matrix &mat2);
+	bool multMatrix(Matrix &mat2);
 	bool transp();
-	int getRows()
-	{
-		return rows;
-	}
-	int getColumns()
-	{
-		return columns;
-	}
+	int getRows() { return rows; }
+	int getColumns() { return columns; }
 	double getElem(int row, int col)
 	{
 		if (row < rows && col < columns && row > -1 && col > -1)
-		{
-			return matr[row][col];
-		}
-		std::cout << "Index error!\n";
+			return mat[row][col];
+		std::cout << "Index error!" << std::endl;
 		return -1;
-	}
-	friend Matrix operator+(Matrix& matrA, Matrix& matrB) {
-		if (matrB.rows == matrA.rows && matrB.columns == matrA.columns)
-		{
-			Matrix res;
-			res.rows = matrA.rows;
-			res.columns = matrA.columns;
-			res.matr = new double*[res.rows];
-			for (int i = 0; i < res.rows; i++)
-			{
-				res.matr[i] = new double[res.columns];
-				for (int j = 0; j < res.columns; j++)
-				{
-					res.matr[i][j] = matrA.matr[i][j] + matrB.matr[i][j];
-				}
-			}
-			return res;
-		}
-		else
-		{
-			std::cout << "Ivalid matrix size!\n";
-		}
-	}
-	friend Matrix operator-(Matrix& matrA, Matrix& matrB) {
-		if (matrB.rows == matrA.rows && matrB.columns == matrA.columns)
-		{
-			Matrix res;
-			res.rows = matrA.rows;
-			res.columns = matrA.columns;
-			res.matr = new double*[res.rows];
-			for (int i = 0; i < res.rows; i++)
-			{
-				res.matr[i] = new double[res.columns];
-				for (int j = 0; j < res.columns; j++)
-				{
-					res.matr[i][j] = matrA.matr[i][j] - matrB.matr[i][j];
-				}
-			}
-			return res;
-		}
-		else
-		{
-			std::cout << "Ivalid matrix size!\n";
-		}
-	}
-	friend Matrix operator*(Matrix& matrA, Matrix& matrB) {
-
-		if (matrA.rows == matrB.columns)
-		{
-			Matrix res;
-			res.columns = matrB.columns;
-			res.rows = matrA.rows;
-			res.matr = new double*[res.rows];
-			for (int k = 0; k < res.columns; k++)
-			{
-				res.matr[k] = new double[res.columns];
-				for (int i = 0; i < res.rows; i++)
-				{
-					for (int j = 0; j < matrB.columns; j++)
-					{
-						res.matr[i][j] += matrA.matr[i][k] * matrB.matr[k][j];
-					}
-				}
-			}	
-			return res;
-		}
-		else
-		{
-			std::cout << "Invalid matrix size!";
-		}
 	}
 };
 class Vector : public Matrix {
-protected:
-	double *vec;
-	int elements;
 public:
-
+	Vector();
+	~Vector();
+	double multVector(Vector &vec2);
+	bool scalVector(double scal);
 };
-int main()
-{
-	Matrix x, y;
-	x.input(); 
-	x.print();
-	y.input();
-	y.print();
-	(x + y).print();
-	(x - y).print();
-	(x * y).print();
-	return 0;
-}
-
 Matrix::Matrix()
 {
+}
+Matrix::Matrix(int n_rows, int n_cols)
+{
+	rows = n_rows;
+	columns = n_cols;
+	mat = new double *[rows];
+	for (int i = 0; i < rows; i++)
+		mat[i] = new double[columns];
+	for (int j = 0; j < rows; j++)
+	{
+		for (int k = 0; k < columns; k++)
+			mat[j][k] = 0;
+	}
 
 }
-
 Matrix::~Matrix()
 {
-
 }
-
 bool Matrix::input()
 {
-	std::cout << "Enter the number of rows and columns: ";
-	std::cin >> rows >> columns;
-	if (rows < 1 || columns < 1)
+	std::cout << "Enter number of rows: ";
+	std::cin >> rows;
+	std::cout << std::endl;
+	std::cout << "Enter number of columns: ";
+	std::cin >> columns;
+	std::cout << std::endl;
+
+	if (rows > 0 && columns > 0)
 	{
-		std::cout << "Invalid parameters!\n";
+		mat = new double *[rows];
+		for (int i = 0; i < rows; i++)
+			mat[i] = new double[columns];
+
+		for (int j = 0; j < rows; j++)
+		{
+			std::cout << "[row " << j + 1 << "]:" << std::endl;
+			for (int k = 0; k < columns; k++)
+				std::cin >> mat[j][k];
+		}
+		std::cout << std::endl;
+		return true;
+	}
+	else
+	{
+		std::cout << "Invalid size!" << std::endl;
+		std::cout << std::endl;
 		return false;
 	}
-	std::cout << '\n';
-	matr = new double*[rows];
-	std::cout << "Enter the elements:\n";
-	for (int i = 0; i < rows; i++)
-	{
-		matr[i] = new double[columns];
-		for (int j = 0; j < columns; j++)
-		{
-			std::cin >> matr[i][j];
-		}
-	}
-	return true;
 }
-
 bool Matrix::print()
 {
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < columns; j++)
-		{
-			std::cout << matr[i][j] << '\t';
-		}
-		std::cout << '\n';
+			std::cout << mat[i][j] << "\t";
+		std::cout << std::endl;
 	}
+	std::cout << std::endl;
 	return true;
 }
-
-bool Matrix::summMatrix(Matrix matr2)
+bool Matrix::summMatrix(Matrix& mat2)
 {
-	if (matr2.getRows() == rows && matr2.getColumns() == columns)
+	if (rows == mat2.getRows() && columns == mat2.getColumns())
 	{
 		for (int i = 0; i < rows; i++)
 		{
 			for (int j = 0; j < columns; j++)
-			{
-				matr[i][j] += matr2.getElem(i, j);
-			}
+				mat[i][j] += mat2.getElem(i, j);
 		}
 		return true;
 	}
 	else
 	{
-		std::cout << "Ivalid matrix size!\n";
+		std::cout << "Invalid summands!" << std::endl;
+		std::cout << std::endl;
 		return false;
 	}
 }
-
-bool Matrix::multMatrix(Matrix matr2)
+bool Matrix::multMatrix(Matrix& mat2)
 {
-	if (columns == matr2.getRows())
+	if (columns == mat2.getRows())
 	{
-		for (int k = 0; k < columns; k++)
+		Matrix temp(this->rows, mat2.getColumns());
+		for (int i = 0; i < rows; i++)
 		{
-			for (int i = 0; i < rows; i++)
+			for (int j = 0; j < mat2.getColumns(); j++)
 			{
-				for (int j = 0; j < matr2.getColumns(); j++)
+				for (int k = 0; k < columns; k++)
 				{
-					matr[i][j] += matr[i][k] * matr2.getElem(k, j);
+					temp.mat[i][j] +=
+						mat[i][k] * mat2.mat[k][j];
 				}
 			}
-
+		}
+		columns = mat2.getColumns();
+		for (int i = 0; i < rows; i++)
+			delete[] mat[i];
+		delete[] mat;
+		mat = nullptr;
+		mat = new double*[temp.getRows()];
+		for (int i = 0; i < temp.getRows(); i++)
+			mat[i] = new double[columns];
+		for (int j = 0; j < rows; j++)
+		{
+			for (int k = 0; k < columns; k++)
+				mat[j][k] = temp.mat[j][k];
 		}
 		return true;
 	}
 	else
 	{
-		std::cout << "Invalid matrix size!\n";
+		std::cout << "Invalid multipliers!" << std::endl;
+		std::cout << std::endl;
 		return false;
 	}
 }
-
 bool Matrix::transp()
 {
-	double tmp;
-	for (int i = 0; i < rows; i++)
+	Matrix temp(this->columns, this->rows);
+	for (int i = 0; i < columns; i++)
 	{
-		for (int j = i + 1; j < columns; j++)
+		for (int j = 0; j < rows; j++)
 		{
-			tmp = matr[i][j];
-			matr[i][j] = matr[j][i];
-			matr[j][i] = tmp;
+			temp.mat[i][j] = mat[j][i];
 		}
+	}
+	for (int i = 0; i < rows; i++)
+		delete[] mat[i];
+	delete[] mat;
+	mat = nullptr;
+	mat = new double*[temp.getRows()];
+	for (int i = 0; i < temp.getRows(); i++)
+		mat[i] = new double[temp.getColumns()];
+	rows = temp.getRows();
+	columns = temp.getColumns();
+	for (int j = 0; j < rows; j++)
+	{
+		for (int k = 0; k < columns; k++)
+			mat[j][k] = temp.mat[j][k];
 	}
 	return true;
 }
+double Vector::multVector(Vector &vec2)
+{
+	double prod = 0;
+	if (this->getColumns() == vec2.getColumns())
+	{
+		for (int i = 0; i < this->getRows(); i++)
+		{
+			for (int j = 0; j < this->getColumns(); j++)
+				prod += this->getElem(i, j) * vec2.getElem(i, j);
+		}
+		return prod;
+	}
+}
+bool Vector::scalVector(double scal)
+{
+	return true;
+}
+Matrix operator+(Matrix& a, Matrix& b)
+{
+	Matrix res(a.getRows(), a.getColumns());
+	for (int i = 0; i < res.getRows(); i++)
+	{
+		for (int j = 0; j < res.getColumns(); j++)
+			res.mat[i][j] = a.getElem(i, j) + b.getElem(i, j);
+	}
+	return res;
+}
+Matrix operator-(Matrix& a, Matrix& b)
+{
+	Matrix res(a.getRows(), a.getColumns());
+	for (int i = 0; i < res.getRows(); i++)
+	{
+		for (int j = 0; j < res.getColumns(); j++)
+			res.mat[i][j] = a.getElem(i, j) - b.getElem(i, j);
+	}
+	return res;
+}
+Matrix operator*(Matrix& a, Matrix& b)
+{
+	Matrix res(a.getRows(), b.getColumns());
+	for (int i = 0; i < a.getRows(); i++)
+	{
+		for (int j = 0; j < b.getColumns(); j++)
+		{
+			for (int k = 0; k < a.getColumns(); k++)
+				res.mat[i][j] += a.getElem(i, k)*b.getElem(k, j);
+		}
+	}
+	return res;
+}
+Vector::Vector() {}
+Vector::~Vector() {}
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main()
+{
+	std::cout << "[Matrices]" << std::endl;
+	Matrix a; Matrix b;
+	a.input(); b.input();
+	std::cout << "---------------------------" << std::endl;
+	std::cout << "Matrix A:" << std::endl;
+	a.print();
+	std::cout << "Matrix B:" << std::endl;
+	b.print();
+	std::cout << "A = A + B =" << std::endl;
+	a.summMatrix(b); a.print();
+	std::cout << "A = A * B =" << std::endl;
+	a.multMatrix(b); a.print();
+	std::cout << "A + B =" << std::endl;
+	(a + b).print();
+	std::cout << "A - B =" << std::endl;
+	(a - b).print();
+	std::cout << "A * B =" << std::endl;
+	(a * b).print();
+	std::cout << "Transposed A =" << std::endl;
+	a.transp(); a.print();
+	std::cout << "Transposed B =" << std::endl;
+	b.transp(); b.print();
+	std::cout << "[Vectors]" << std::endl;
+	Vector c, d;
+	c.input(); d.input();
+	std::cout << "Vector C =" << std::endl;
+	c.print();
+	std::cout << "Vector D =" << std::endl;
+	d.print();
+	double prod = c.multVector(d);
+	std::cout << "C * D = " << prod << std::endl;
+	std::cout << "C + D = " << std::endl;
+	(c + d).print();
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	return 0;
+}
