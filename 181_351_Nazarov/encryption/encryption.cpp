999bf09fbc20244650cf8736598128d8becb3787
spoff <evenstruct0@gmail.com>
2019-01-21 07:52:53 +0300

    encryption

---
+++ 181_351_Nazarov/encryption/encryption.cpp
@@ -0,0 +1,66 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+#include <openssl/conf.h> // OpenSSL config
+#include <openssl/evp.h> // crypto functions
+#include <openssl/err.h> // errors
+#include <openssl/aes.h> // AES implementation
+#include <fstream>
+
+#pragma comment(lib,"libcrypto.lib")
+
+int main()
+// message --> crypt --> decrypt
+{
+	std::ifstream plaintext_f;
+	std::ofstream ciphertext_f;
+	plaintext_f.open("message.txt", std::fstream::in | std::fstream::binary);
+	ciphertext_f.open("crypt.txt", std::fstream::out | std::fstream::binary);
+	unsigned char *key = (unsigned char *)"0123456789";
+	unsigned char *iv = (unsigned char *)"0123456789012345";
+	char in_buf[256] = { 0 };
+	char out_buf[256] = { 0 };
+	int len = 0;
+	EVP_CIPHER_CTX *ctx;
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+	plaintext_f.read(in_buf, 256); // reading to buffer
+	// encryption cycle:
+	while (plaintext_f.gcount() > 0)
+	{
+		EVP_EncryptUpdate(ctx, (unsigned char *)out_buf, &len,
+			(unsigned char *)in_buf, plaintext_f.gcount()); // encrypting...
+		ciphertext_f.write((const char *)out_buf, len);
+		plaintext_f.read(in_buf, 256);
+	}
+	EVP_EncryptFinal_ex(ctx, (unsigned char *)out_buf, &len); // finalizing func
+	EVP_CIPHER_CTX_free(ctx);
+	ciphertext_f.write((const char*)out_buf, len); // write the final block to file
+	plaintext_f.close();
+	ciphertext_f.close();
+	memset(in_buf, 0, 256);
+	memset(out_buf, 0, 256);
+
+	std::ifstream crypt_f;
+	std::ofstream decrypt_f;
+	crypt_f.open("crypt.txt", std::fstream::in | std::fstream::binary);
+	decrypt_f.open("decrypt.txt", std::fstream::out | std::fstream::binary);
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+	crypt_f.read(in_buf, 256);
+	// decryption cycle
+	while (crypt_f.gcount() > 0)
+	{
+		EVP_DecryptUpdate(ctx, (unsigned char *)out_buf, &len,
+			(unsigned char *)in_buf, crypt_f.gcount());
+		decrypt_f.write((const char *)out_buf, len);
+		crypt_f.read(in_buf, 256);
+	}
+	EVP_DecryptFinal_ex(ctx, (unsigned char *)out_buf, &len);
+	decrypt_f.write((const char *)out_buf, len);
+	decrypt_f.close();
+	crypt_f.close();
+
+	system("pause");
+	return 0;
+}
--- 181_351_Nazarov/tree/tree.cpp
+++ 181_351_Nazarov/tree/tree.cpp
@@ -2,61 +2,64 @@
 #include <iostream>
 
 struct Node {
-	int value;
-	Node *son, *bro;
+	int value; // data to be stored
+	Node *son, *bro; // pointers to neigbor and lower level
 };
 
 class Tree {
-	Node *root;
-	Node *x_ptr, *y_ptr;
+	Node *root; // first element of a tree
+	Node *x_ptr, *y_ptr; // x points to the last element on the current level
+						 // y points to the first element of the current level
 public:
-	void NewTree();
-	void DelTree(Node *d);
-	Tree() { NewTree(); }
-	~Tree() { DelTree(root); }
-	void InsertRoot(int val);
-	void AddLevel(int val);
-	void AddNext(int val);
-	void PrintTree(Node *r);
-	Node *getRoot() { return root; }
+	void NewTree(); // function for the constructor (sets all the pointers equal to NULL)
+	void DelTree(Node *d); // function for the destructor (recursively deletes the tree
+	Tree() { NewTree(); } // constructor
+	~Tree() { DelTree(root); } // destructor
+	void InsertRoot(int val); // inserts the first value	
+	void AddLevel(int val); // adds new level (y axis)
+	void AddNext(int val); // adds new element to the current level (x axis)
+	void PrintTree(Node *r); // recursively prints the tree
+	Node *getRoot() { return root; } // returns root pointer
 };
-
 void Tree::NewTree()
 {
 	x_ptr = y_ptr = root = NULL;
 }
 void Tree::InsertRoot(int val)
 {
-	root = new(Node);
-	x_ptr = y_ptr = root;
-	root->value = val;
-	x_ptr->bro = y_ptr->son = NULL;
+	root = new(Node); // now root points to smth
+	x_ptr = y_ptr = root; // and other pointers do
+	root->value = val; // insert a value
+	x_ptr->bro = y_ptr->son = NULL; // set all other pointers NULL
 }
 void Tree::AddLevel(int val)
 {
-	y_ptr->son = new(Node);
-	y_ptr = x_ptr = y_ptr->son;
+	y_ptr->son = new(Node); // now an element has a son
+	y_ptr = x_ptr = y_ptr->son; // a new level is created
 	y_ptr->value = val;
 	y_ptr->bro = y_ptr->son = NULL;
 
 }
 void Tree::AddNext(int val)
 {
-	x_ptr->bro = new(Node);
+	x_ptr->bro = new(Node); // now an element has a bro
 	x_ptr = x_ptr->bro;
 	x_ptr->value = val;
 	x_ptr->bro = x_ptr->son = NULL;
 }
+
 void Tree::PrintTree(Node *leaf)
 {
 	if (!leaf)
 		return;
-	std::cout << leaf->value << '\t';
+	std::cout << leaf->value << "\t";
 	PrintTree(leaf->bro);
-	if (leaf->son == NULL && leaf->bro == NULL || leaf->son != NULL && leaf->bro == NULL)
+	if (leaf->son == NULL && leaf->bro == NULL || // if last on the level
+		leaf->son != NULL && leaf->bro == NULL)
 		std::cout << "\n\n";
 	PrintTree(leaf->son);
 }
+
 void Tree::DelTree(Node *leaf)
 {
 	if (leaf != 0)
@@ -68,8 +71,8 @@ void Tree::DelTree(Node *leaf)
 }
 int main()
 {
-	int r_val, l_val;
-	char ch;
+	int r_val, l_val; // vars for values
+	char ch;		// for the interpreter
 	std::cout << "Let's build a tree.\n\nPlease, enter a root value: ";
 	std::cin >> r_val;
 	Tree db;
